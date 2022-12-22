769fa1df1d34f412cb5257fb874a6c70c293c99c
Z3C4R <sava.p@hotmail.com>
2022-12-22 13:49:01 +0100

    2# Testiranje povezanosti sa bazom(404, 405)

--- Backend/Engine.py
+++ Backend/Engine.py
@@ -1,13 +1,44 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 Engine=Flask(__name__)
-Engine.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+Engine.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1234@localhost/Portfolio'
 db=SQLAlchemy(Engine)
 
-Engine.route('/')
+class Event(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    description=db.Column(db.String(100), nullable=False)
+    created_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Event: {self.description}"
+
+    def __init__(self, description):
+        self.description=description
+
+def format_event(event):
+    return{
+        "description":event.description,
+        "id":event.id,
+        "created_at": event.created_at
+    }
+
+@Engine.route('/')
 def hello():
     return 'Hey!'
+
+@Engine.route('/event', methods=['POST'])
+def create_event():
+    description=request.json['description']
+    event=Event(description)
+    db.session.add(event)
+    db.session.commit()
+    return format_event(event)
+
+@Engine.route('/events', method=['GET'])
+def get_events():
+    events=Event.query.order_by(Event.id.asc()).all()    
 
 if __name__=='__main__':
     Engine.run()
