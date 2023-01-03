5b8d494af84059f1d8f7b12412218b2ce7ecd1a0
antonic <aleksandar.antonic00@gmail.com>
2023-01-03 23:51:31 +0100

    login, register

---
+++ frontend/src/templates/index.js
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./index.css";
+import Login from "./login";
+
+function Index() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+			    <a href="/">Home</a>
+				<a href="/Login">Login</a>
+				<a href="/Register">Register</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Index;
