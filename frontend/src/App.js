284624504533913e546401a6419e7b70fb3891b2
Rale27 <r.tesovic27@gmail.com>
2023-01-11 18:21:09 +0100

    cryptoCoin api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import Login from './templates/login';
 import Register from './templates/register';
 import Navbar from './templates/navbar';
+import CoinNavbar from './components/CoinNavbar';
+import Coins from './components/Coins';
 import Home from './templates/home';
-import React, {useMemo, useState} from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 import axios from "axios";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import {UserContext} from './UserContext';
@@ -14,11 +16,29 @@ export default function App() {
 
   const userValue= useMemo(()=>({currentUser, setCurrentUser}),[currentUser,setCurrentUser]);
 
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+ 
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data[0])
+
+    }).catch((error) => {
+      console.log(error)
+    })
+
+  }, [])
+  
+
   return (
 
     <BrowserRouter>
       <UserContext.Provider value={userValue}>
       <Navbar />
+      <CoinNavbar />
+      <Coins coins={coins} />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />} />
