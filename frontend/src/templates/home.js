29a01c932d495d42e4b199380161a086b1d718ab
antonic <aleksandar.antonic00@gmail.com>
2023-01-19 13:49:47 +0100

    search filter

--- frontend/src/templates/home.js
+++ frontend/src/templates/home.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CoinItem from './CoinItem'
 import './Coins.css'
 import Coin from '../routes/Coin'
 import { Link } from 'react-router-dom'
 
 const Home = (props) => {
+
+    // search
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleChange = (e) => {
+      e.preventDefault();
+      setSearchInput(e.target.value);
+    };
+
     return (
       <div className ='container'> 
-          <div>
+        <div >
+          <input type="text" placeholder="Search here" 
+            onChange={handleChange} value={searchInput} />
+         </div>
+            <div>
               <div className='heading'>
               <p>#</p>
               <p className='coin-name'>Coin</p>
@@ -17,14 +30,16 @@ const Home = (props) => {
               <p className='hide-mobile'>Mkt Cap</p>
               </div>
 
-              {props.coins.map(coins =>{
+              {props.coins.filter((coin) => coin.name.toLowerCase().includes(searchInput)).
+              map(coins =>{
                 return(
                   <Link to={`/coin/${coins.id}`} element ={<Coin />} key={coins.id}>
                     <CoinItem coins={coins}/> 
                   </Link>
                 )
               })}
-          </div>
+           </div>
+           
       </div>
     )
   } 
--- frontend/src/templates/logout.js
+++ frontend/src/templates/logout.js
@@ -5,11 +5,9 @@ import { Navigate } from "react-router-dom";
 function Logout(){
 
     const {currentUser,setCurrentUser}= useContext(UserContext);
-
-    console.log(currentUser);
+    
     setCurrentUser(null);
-    console.log(currentUser);
-
+    
     return(
         <Navigate to="/login" replace={true} />
         )
